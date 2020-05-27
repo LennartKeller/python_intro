c361699f75f5083ff044518103b94e7889f6df5d
Lennart Keller <lennart.keller@googlemail.de>
2020-05-27 12:28:37 +0200

    add import explantation file

---
+++ bad_chars.py
@@ -0,0 +1,11 @@
+import time
+
+for t in reversed(range(1,11)):
+	print(f"Nur noch {t} Sekund{'en' if t > 1 else 'e'}. Dann ist alles startbereit!")
+	time.sleep(1)
+
+def get_chars(instring):
+    chars = []
+    for i in instring:
+        chars.append(i)
+    return chars
